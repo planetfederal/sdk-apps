cad1a72d8ecdd51a34f70b4ac44ab329892c054b
Bart van den Eijnden <bartvde@boundlessgeo.com>
2015-12-14 12:31:59 +0100

    The returning of style objects is not yet in an ol3 release

--- basic/app.jsx
+++ basic/app.jsx
@@ -89,7 +89,7 @@ var stylePopp = function(feature) {
   if (size !== 1) {
     style = clusterStyleCachePopp[size];
     if (!style) {
-      style = new ol.style.Style({
+      style = [new ol.style.Style({
         image: new ol.style.Circle({
           radius: 10,
           stroke: new ol.style.Stroke({
@@ -105,7 +105,7 @@ var stylePopp = function(feature) {
             color: '#fff'
           })
         })
-      });
+      })];
       clusterStyleCachePopp[size] = style;
     }
     return style;
