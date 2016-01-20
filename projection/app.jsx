6168b7fef932fc0c950f193c253796d677d0d214
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-01-20 17:09:14 +0100

    Update to ol 3.13

--- projection/app.jsx
+++ projection/app.jsx
@@ -8,8 +8,7 @@ import proj4 from 'proj4';
 import InfoPopup from './node_modules/boundless-sdk/js/components/InfoPopup.jsx';
 import App from './node_modules/boundless-sdk/js/components/App.js';
 
-// TODO can be removed with new ol3 release
-global.proj4 = proj4;
+ol.proj.setProj4(proj4);
 
 proj4.defs('EPSG:28992', '+proj=sterea +lat_0=52.15616055555555 +lon_0=5.38763888888889 +k=0.9999079 +x_0=155000 +y_0=463000 +ellps=bessel +units=m +towgs84=565.2369,50.0087,465.658,-0.406857330322398,0.350732676542563,-1.8703473836068,4.0812 +no_defs');
 
