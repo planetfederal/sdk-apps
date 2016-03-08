9c67dfd6a6af0f9968a8b9614a722bc373cb45e5
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-03-08 11:40:14 +0100

    Create package for simple app

--- simple/app.jsx
+++ simple/app.jsx
@@ -24,7 +24,7 @@ var styleAirports = new ol.style.Style({
     anchorXUnits: 'fraction',
     anchorYUnits: 'fraction',
     anchor: [0.5, 0.5],
-    src: '../../data/styles/plane.svg'
+    src: './data/styles/plane.svg'
   })
 });
 
@@ -136,7 +136,7 @@ var map = new ol.Map({
       style: styleTrees,
       source: new ol.source.Vector({
         format: new ol.format.GeoJSON(),
-        url: '../../data/trees.json'
+        url: './data/trees.json'
       })
     }),
     new ol.layer.Vector({
@@ -147,7 +147,7 @@ var map = new ol.Map({
       source: new ol.source.Cluster({
         source: new ol.source.Vector({
           format: new ol.format.GeoJSON(),
-          url: '../../data/popp.json'
+          url: './data/popp.json'
         })
       })
     }),
@@ -158,7 +158,7 @@ var map = new ol.Map({
       style: styleAirports,
       source: new ol.source.Vector({
         format: new ol.format.GeoJSON(),
-        url: '../../data/airports.json'
+        url: './data/airports.json'
       })
     })
   ],
