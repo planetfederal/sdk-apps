328f06cff5c2a21651d6abce542eb0cbc09a8a3a
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-04-28 15:32:11 +0200

    Upgrade app-features to new UI

--- app-features/app.jsx
+++ app-features/app.jsx
@@ -5,11 +5,18 @@ import {addLocaleData, IntlProvider} from 'react-intl';
 import App from './node_modules/boundless-sdk/js/components/App.js';
 import HomeButton from './node_modules/boundless-sdk/js/components/HomeButton.jsx';
 import LayerList from './node_modules/boundless-sdk/js/components/LayerList.jsx';
-import Toolbar from './node_modules/boundless-sdk/js/components/Toolbar.jsx';
 import EditPopup from './node_modules/boundless-sdk/js/components/EditPopup.jsx';
+import AppBar from 'material-ui/lib/app-bar';
 import WFST from './node_modules/boundless-sdk/js/components/WFST.jsx';
 import enLocaleData from './node_modules/react-intl/locale-data/en.js';
 import enMessages from './node_modules/boundless-sdk/locale/en.js';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+
+// Needed for onTouchTap
+// Can go away when react 1.0 release
+// Check this repo:
+// https://github.com/zilverline/react-tap-event-plugin
+injectTapEventPlugin();
 
 addLocaleData(
   enLocaleData
@@ -48,20 +55,15 @@ var map = new ol.Map({
 
 class MyApp extends App {
   render() {
-    var options = [{
-      exclude: true,
-      pullRight: false,
-      jsx: (<article><img src="resources/logo.svg" width="30" height="30"></img><span className='app-title'>Features</span></article>)
-    }];
     return (
-      <article>
-        <Toolbar options={options} />
+      <div id='content'>
+        <AppBar iconElementLeft={<img style={{marginTop: '10px'}} src="resources/logo.svg" width="30" height="30" />} title="Features" />
         <div ref='map' id='map'></div>
         <div><LayerList allowStyling={true} showOnStart={true} showZoomTo={true} allowEditing={true} allowReordering={true} addLayer={{url: '/geoserver/wfs?', asVector: true}} expandOnHover={false} map={map} /></div>
-        <div id='home-button' className='ol-unselectable ol-control'><HomeButton map={map} /></div>
+        <div id='home-button'><HomeButton map={map} /></div>
         <div id='popup' className='ol-popup'><EditPopup map={map} /></div>
         <div id='wfst'><WFST layerSelector={false} visible={false} map={map} /></div>
-      </article>
+      </div>
     );
   }
 }
