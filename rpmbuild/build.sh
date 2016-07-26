59768f35303659124a3198e42005683edb651f9c
Nick Stires <nstires@boundlessgeo.com>
2016-07-26 13:16:26 -0400

    Change docs location

--- rpmbuild/build.sh
+++ rpmbuild/build.sh
@@ -32,8 +32,8 @@ done
 cp SPECS/${COMPONENT}.spec $COMPONENT/SPECS
 mkdir -p $COMPONENT/SRC/opt/boundless/suite/quickview
 unzip ../archive/quickview.war -d $COMPONENT/SRC/opt/boundless/suite/quickview/
-#mkdir -p $COMPONENT/SRC/opt/boundless/suite/quickview/doc
-#cp $WORKSPACE/rpmbuild/LICENSE.md $COMPONENT/SRC/opt/boundless/suite/quickview/doc
+mkdir -p $COMPONENT/SRC/usr/share/doc/
+mv $COMPONENT/SRC/opt/boundless/suite/quickview/doc $COMPONENT/SRC/usr/share/doc/$COMPONENT
 mkdir -p $COMPONENT/SRC/etc/tomcat8/Catalina/localhost/
 cp tomcat-context/quickview.xml $COMPONENT/SRC/etc/tomcat8/Catalina/localhost/
 
