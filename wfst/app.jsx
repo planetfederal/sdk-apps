a4abac4a38e35391b1fe32df855e7f3561626760
Bart van den Eijnden <bartvde@boundlessgeo.com>
2016-10-25 17:25:22 +0200

    Attribution html should be a plain string

--- wfst/app.jsx
+++ wfst/app.jsx
@@ -78,7 +78,7 @@ var map = new ol.Map({
           source: new ol.source.XYZ({
             attributions: [
               new ol.Attribution({
-                html:['Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community']
+                html: 'Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community'
               })
             ],
             url: 'http://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'
