2d5afe44406c44381f8d520476e6c1fafe799f2f
bartvde <bartvde@boundlessgeo.com>
2017-02-22 17:14:38 +0100

    Start with one app at a time so we can make publish work

--- test.sh
+++ test.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
-npm config set loglevel warn
-for i in $( ls **/app.jsx ); do
-  dir="$(dirname $i)"
-  cd $dir
-  npm i
-  npm run test
-  cd ..
-done
-for i in $( ls **/**/app.jsx ); do
-  dir="$(dirname $i)"
-  cd $dir
-  npm i
-  npm run test
-  cd ../../
-done
+cd basic
+npm i
+npm run test
+cd ..
