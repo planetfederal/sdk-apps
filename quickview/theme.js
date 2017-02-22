490b79062439845b75fb455839f9c27f3854297b
bartvde <bartvde@boundlessgeo.com>
2017-02-22 13:15:21 +0100

    Start with empty gh-pages branch

--- quickview/theme.js
+++
@@ -1,9 +0,0 @@
-import {cyan700, lime500} from 'material-ui/styles/colors';
-
-export default {
-  palette: {
-    primary1Color: cyan700,
-    active1Color: lime500,
-    accent1Color: cyan700
-  }
-};
