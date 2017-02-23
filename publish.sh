ff6e8f53b0a28bd73d9a3fba4a9ff730cd0d3d71
bartvde <bartvde@boundlessgeo.com>
2017-02-23 09:55:21 +0100

    Try looping over apps

--- publish.sh
+++ publish.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-mkdir -p out/basic
-cd basic
-npm i --loglevel silent > /dev/null
-npm run build
-npm run createzip -- --output-file=../out/basic/package.zip
-cd ../out/basic
-unzip package.zip
-rm package.zip
-cd ..
+for i in $( ls **/app.jsx ); do
+  dir="$(dirname $i)"
+  mkdir -p out/$dir
+  cd $dir
+  npm i --loglevel silent > /dev/null
+  npm run build
+  npm run createzip -- --output-file=../out/$dir/package.zip
+  cd ../out/$dir
+  unzip package.zip
+  rm package.zip
+  cd ..
+done
--- test.sh
+++ test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e # Exit with nonzero exit code if anything fails
-cd basic
-npm i --loglevel silent > /dev/null
-npm run test
-cd ..
+for i in $( ls **/app.jsx ); do
+  dir="$(dirname $i)"
+  cd $dir
+  npm i --loglevel silent > /dev/null
+  npm run test
+  cd ..
+done
